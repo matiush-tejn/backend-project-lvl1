ec898e8ef1bbe2a09769e39267e0add411fd6fdf
matiush-tejn <matiush-tejn@yandex.ru>
2019-07-16 18:00:12 +0500

    implemented game brain-even

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import readlineSync from 'readline-sync';
 
+const getRandomNum = (min, max) => Math.floor(min + Math.random() * (max - min + 1));
+
 export default () => {
-  console.log('Welcome to the Brain Games!\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+
+  const iter = (count) => {
+    if (count === 3) return `Congratulations, ${playerName}!`;
+    const num = getRandomNum(0, 100);
+    const correctAnswer = num % 2 === 0 ? 'yes' : 'no';
+    const playerAnswer = readlineSync.question(`Question: ${num}\nYour answer: `);
+    if (correctAnswer !== playerAnswer) {
+      return `'${playerAnswer}' is wrong answer ;). Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`;
+    }
+    console.log('Correct!');
+    return iter(count + 1);
+  };
+
+  console.log(iter(0));
 };
