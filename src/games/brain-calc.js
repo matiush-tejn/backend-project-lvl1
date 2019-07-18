73bd361dd6bb2f28c14fa1d901ec5aa770d1f99a
matiush-tejn <matiush-tejn@yandex.ru>
2019-07-18 10:25:16 +0500

    refactoring

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,12 +3,16 @@ import { getRandomNum } from '../utils';
 
 const description = 'What is the result of the expression?';
 
-const getOperationData = () => [['+', (a, b) => a + b], ['-', (a, b) => a - b], ['*', (a, b) => a * b]][getRandomNum(0, 2)];
+const operations = [
+  ['+', (a, b) => a + b],
+  ['-', (a, b) => a - b],
+  ['*', (a, b) => a * b],
+];
 
 const getGameData = () => {
   const num1 = getRandomNum(0, 99);
   const num2 = getRandomNum(0, 99);
-  const [operationSign, operation] = getOperationData();
+  const [operationSign, operation] = operations[getRandomNum(0, operations.length - 1)];
   const question = `${num1} ${operationSign} ${num2}`;
   const rightAnswer = `${operation(num1, num2)}`;
   return [question, rightAnswer];
