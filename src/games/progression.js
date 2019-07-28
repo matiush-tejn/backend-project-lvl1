3b5f9c9efc048e3454be1d41bf815509fa164a1f
matiush-tejn <matiush-tejn@yandex.ru>
2019-07-28 11:18:57 +0500

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,6 +3,8 @@ import getRandomNum from '../utils';
 
 const description = 'What number is missing in the progression?';
 
+const length = 10;
+
 const getProgression = (first, step, len) => {
   const iter = (num, acc) => (
     acc.length < len ? iter(num + step, [...acc, num]) : acc);
@@ -12,9 +14,8 @@ const getProgression = (first, step, len) => {
 const getGameData = () => {
   const first = getRandomNum(0, 99);
   const step = getRandomNum(1, 9);
-  const len = 10;
-  const elements = getProgression(first, step, len);
-  const hiddenElementIndex = getRandomNum(0, len - 1);
+  const elements = getProgression(first, step, length);
+  const hiddenElementIndex = getRandomNum(0, length - 1);
   const question = elements.map((num, i) => (i === hiddenElementIndex ? '..' : num)).join(' ');
   const rightAnswer = elements[hiddenElementIndex].toString();
   return [question, rightAnswer];
